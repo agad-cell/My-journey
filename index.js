dda00f4974687c6d7b6f1f5d9b0800ee0a8b9408
agad <saiqabaloch79@gmail.com>
2026-08-09 07:06:18 +0500

    js logic half done and html layout created

---
+++ index.js
@@ -0,0 +1,40 @@
+// function calculator() {
+//     let input = Number(prompt("enter a number"));
+// let operator= prompt("Enter the operator");
+// let secInput = Number(prompt("enter a second number"));
+// if (operator==="+") {
+//     result = (input + secInput);
+//     console.log(result);
+// }
+// else if (operator==="-") {
+//  result = (input - secInput);
+//   console.log(result);
+// }
+// else if (operator==="*") {
+//     result = (input * secInput);
+//      console.log(result);
+// }
+// else if (operator==="/") {
+//    result = (input / secInput);
+//     console.log(result);
+// }
+// else if (operator==="%") {
+//     result = (input % secInput);
+//      console.log(result);
+// }
+// else {
+//     result= ("invalid operator");
+// }
+
+// }
+// calculator();
+
+// let again;
+// again = prompt("do u want again");
+ 
+// while (again === "yes") {
+// calculator();
+// break;
+// }
+
+
