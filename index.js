3fe3fb1f9147d2b8d9d15843c6ffbde720e14cb2
agad <saiqabaloch79@gmail.com>
2026-08-10 15:40:26 +0500

    Clear the text show on display

--- index.js
+++ index.js
@@ -1,33 +1,33 @@
-// function calculator() {
-//     let input = Number(prompt("enter a number"));
-// let operator= prompt("Enter the operator");
-// let secInput = Number(prompt("enter a second number"));
-// if (operator==="+") {
-//     result = (input + secInput);
-//     console.log(result);
-// }
-// else if (operator==="-") {
-//  result = (input - secInput);
-//   console.log(result);
-// }
-// else if (operator==="*") {
-//     result = (input * secInput);
-//      console.log(result);
-// }
-// else if (operator==="/") {
-//    result = (input / secInput);
-//     console.log(result);
-// }
-// else if (operator==="%") {
-//     result = (input % secInput);
-//      console.log(result);
-// }
-// else {
-//     result= ("invalid operator");
-// }
+function calculator() {
+    let input;
+let operator;
+let secInput;
+if (operator==="+") {
+    result = (input + secInput);
+    console.log(result);
+}
+else if (operator==="-") {
+ result = (input - secInput);
+  console.log(result);
+}
+else if (operator==="*") {
+    result = (input * secInput);
+     console.log(result);
+}
+else if (operator==="/") {
+   result = (input / secInput);
+    console.log(result);
+}
+else if (operator==="%") {
+    result = (input % secInput);
+     console.log(result);
+}
+else {
+    result= ("invalid operator");
+}
 
-// }
-// calculator();
+}
+calculator();
 
 // let again;
 // again = prompt("do u want again");
@@ -38,3 +38,10 @@
 // }
 
 
+let mainFunc = document.querySelectorAll('btn');
+//   mainFunc.addEventListener('click', calculator);
+mainFunc.forEach(function(button) {
+    mainFunc.addEventListener("click", function() {
+        console.log("You clicked:", button.textContent);
+    });
+});
